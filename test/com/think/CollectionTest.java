9217f13eb4b4695bfad8583964c426330cc0bd63
zhang_yx <zhang_yx@gzhc365.com>
2021-04-06 22:13:25 +0800

    添加单测

---
+++ test/com/think/CollectionTest.java
@@ -0,0 +1,24 @@
+package com.think;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Auther: zhang_yx
+ * @Date: 2021/4/6 21:45
+ */
+public class CollectionTest {
+
+    @org.junit.Test
+    public void forEach() {
+        List<String> list = new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        for (Iterator<String> iterator = list.iterator(); iterator.hasNext(); ) {
+            System.out.println(iterator.next());
+        }
+    }
+}
