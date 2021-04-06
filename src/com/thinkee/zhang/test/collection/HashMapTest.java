7f8d4c501c020131764da8f78f4e69644fb9538d
zhang_yx <zhang_yx@gzhc365.com>
2021-04-06 21:03:53 +0800

    初始工程搭建

---
+++ src/com/thinkee/zhang/test/collection/HashMapTest.java
@@ -0,0 +1,18 @@
+package com.thinkee.zhang.test.collection;
+
+import java.util.HashMap;
+
+/**
+ * @Auther: zhang_yx
+ * @Date: 2021/4/5 22:17
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+
+        HashMap map = new HashMap();
+        map.put("1","2");
+
+    }
+
+}
